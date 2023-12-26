1f60b6252bb50ece4b80d03481cf2ba06a05bb2e
JJC3321 <122761865+JJC3321@users.noreply.github.com>
2023-12-26 00:26:50 -0500

    Update multiplesof3and5.cpp

--- Solution/multiplesof3and5.cpp
+++ Solution/multiplesof3and5.cpp
@@ -2,15 +2,14 @@
 
 using namespace std;
 
-//Problem
+
 /*
+Problem: 
 If we list all the natural numbers below 10 that are multiples of 
 3 or 5, we get 3.5, 6, and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
-*/
 
-
-/*
+Solution:
 Using the initial count of 23 for all sums multiples of 3 or 5 
 we then use a for loop to add any natural numbers that are
 multiples of 3 or 5 until we reach 1000. Then, get the total.
